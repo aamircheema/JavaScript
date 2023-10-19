9a58a3a6ed6b7235b04e5e8e75e7be2fd2528c86
Aamir Cheema <amircheema295@gmail.com>
2023-10-19 14:54:32 +0500

    Data types

---
+++ datatypes/datatypes.js
@@ -0,0 +1,14 @@
+/* Data types in JavaScript
+String, Number, Null, undefined, BigInt, Symbol, Boolean
+*/
+let name = "Aamir";
+let age = 20;
+let marks = null;
+let salary = undefined;
+let isEmploy = true;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof marks); // object
+console.log(typeof salary); // undefined
+console.log(typeof isEmploy); /// boolean
